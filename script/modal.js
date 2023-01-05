cff466cf54579bedc658fe4e9cd61d3f7b9344bf
Mac <trongnhan34vn@gmail.com>
2023-01-05 08:28:07 +0700

    update

--- script/modal.js
+++ script/modal.js
@@ -7,21 +7,28 @@ function editBtn() {
     
     for (let i = 0; i < editBtn.length; i++) {
         editBtn[i].addEventListener('click', function() {
-            console.log("aaaaaaa");
             document.getElementById('overlay').classList.add('is-visible');
             document.getElementById('modal').classList.add('is-visible');
           });
     }
-    document.getElementById('close-btn').addEventListener('click', function() {
-      document.getElementById('overlay').classList.remove('is-visible');
-      document.getElementById('modal').classList.remove('is-visible');
-    });
-    document.getElementById('overlay').addEventListener('click', function() {
-      document.getElementById('overlay').classList.remove('is-visible');
-      document.getElementById('modal').classList.remove('is-visible');
-    });
 }
 
 editBtn()
+
+function closeBtn() {
+    document.getElementById('close-btn').addEventListener('click', function() {
+        console.log("aaaaaaa");
+        document.getElementById('overlay').classList.remove('is-visible');
+        document.getElementById('modal').classList.remove('is-visible');
+      });
+      document.getElementById('overlay').addEventListener('click', function() {
+        console.log("bbbbbbbb");
+        document.getElementById('overlay').classList.remove('is-visible');
+        document.getElementById('modal').classList.remove('is-visible');
+      });
+}
+
+closeBtn()
+
 
   
